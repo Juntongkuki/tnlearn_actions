bf27012afc1287f2ec516d5e9d9717b99d9c7fd9
Juntongkuki <73333678+Juntongkuki@users.noreply.github.com>
2024-04-18 21:56:32 +0800

    Update __init__.py

--- tnlearn/__init__.py
+++ tnlearn/__init__.py
@@ -1,11 +1,6 @@
-#from tnlearn.regressor import VecSymRegressor
-#from tnlearn.mlpregressor import MLPRegressor
-#from tnlearn.mlpclassifier import MLPClassifier
-#from tnlearn.preprocessing import DataPreprocessor
-#from tnlearn.base import BaseModel
+from tnlearn.regressor import VecSymRegressor
+from tnlearn.mlpregressor import MLPRegressor
+from tnlearn.mlpclassifier import MLPClassifier
+from tnlearn.preprocessing import DataPreprocessor
+from tnlearn.base import BaseModel
 
-from tnlearn.tnlearn.regressor import VecSymRegressor
-from tnlearn.tnlearn.mlpregressor import MLPRegressor
-from tnlearn.tnlearn.mlpclassifier import MLPClassifier
-from tnlearn.tnlearn.preprocessing import DataPreprocessor
-from tnlearn.tnlearn.base import BaseModel
